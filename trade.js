86e2578e8f0aced3799d69f1466b5132681c6880
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2016-02-29 00:02:32 +0200

    add co-mysql

--- trade.js
+++ trade.js
@@ -1,8 +1,17 @@
-var config = require('config');
+var config = require('config'),
+    sql = require('co-mysql').createPool(config.get("sql"))
+    ;
 
 module.exports = {
     main: function *(next) {
-        this.body = config.get('sql');
+        this.body = yield sql.query('select now()');
+        //if (!this.session.user_id) return this.redirect('/login/');
+        //var items = yield sql.query('SELECT * FROM mail.mail LEFT JOIN mail.users ON user_from = user_id where user_to=? and del=0 order by id desc limit 6', [this.session.user_id]);
+        //yield this.render('main', {
+        //    session: this.session,
+        //    page: 'main',
+        //    items: items
+        //});
         yield next;
     },
 }
